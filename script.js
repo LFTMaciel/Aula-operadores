6452de0db64de036993a9da5b1c4b0d4cd3a4208
Maciel <lftmaciel83@gmail.com>
2023-04-12 21:45:56 -0300

    aula do dia 12/04/2022 - exercicios de operadores

---
+++ script.js
@@ -0,0 +1,24 @@
+// Graus Fahrenheit(ºF) para Kelvin(k)
+
+//(Kelvin) = (Graus_Fahrenheit - 32)*(5/9) + 273.15
+
+// Graus Celsius(ºC) para Graus Fahrenheit(ºF)
+
+// (Graus_Fahrenheit) = (Graus_Celsius)*(9/5) + 32
+
+/*
+let Kelvin = (77 - 32)*(5/9) + 273.15
+console.log(Kelvin + "°K")
+
+let Celsius = (80)*(9/5) + 32
+console.log(Celsius + "°C")
+
+let Fahrenheit = (30)*(9/5) + 32
+console.log(Fahrenheit + "°F")
+
+let KelvinX = (30 - 32)*(5/9) + 273.15
+console.log(KelvinX + "°K")
+*/
+let Fahrenheit = parseFloat (prompt("Digite a temperatura em Celsius que queira converter em Fahrenheit"))
+console.log(Fahrenheit) * (9/5) + 32
+
